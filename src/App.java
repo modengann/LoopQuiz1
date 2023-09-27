fa80395531ab4193c0d6dc5719935ec27c352ba1
modengann <99417989+modengann@users.noreply.github.com>
2023-09-26 20:27:58 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,14 @@
+/**
+ *  Create a Java program that asks the user to enter ages of people until the user enters -1. Make sure 
+ *  each age entered is above 0. If it is less than 0 (but not -1), ask them to reenter the age. After 
+ *  the user enters -1, display the oldest person's age and the average age.
+ *  */ 
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
